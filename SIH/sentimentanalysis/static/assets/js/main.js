2cb36ff448885bcf8a7e12f8414d04a7859aae21
chamow97 <jchandramowli15039@it.ssn.edu.in>
2018-03-29 15:47:20 +0530

    Add all maps

--- SIH/sentimentanalysis/static/assets/js/main.js
+++ SIH/sentimentanalysis/static/assets/js/main.js
@@ -1,3 +1,5 @@
+
+//smooth scroll
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000, // Integer. How fast to complete the scroll in milliseconds
 	easing: 'easeInOutCubic', // Easing pattern to use
@@ -6,28 +8,17 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	},
 });
 
+function showTrending(){
+    var trendingBox = document.querySelector('#trending-div');
+    if(trendingBox.classList.contains('zero-opacity')){
+        trendingBox.classList.remove('zero-opacity');
+        trendingBox.classList.add('trending-box');
+    }
+    else{
+        trendingBox.classList.remove('trending-box');
+        trendingBox.classList.add('zero-opacity');
+    }
+}
 
-// var ctx = document.getElementById("sentiment-chart");
-// var sentimentChart = new Chart(ctx, {
-//     type: 'pie',
-//     data: {
-//         labels: ["Positive", "Negative", "Neutral"],
-//         datasets: [{
-//             label: '% of Tweets',
-//             data: [60, 20, 20],
-//             backgroundColor: [
-//                 "#4CD900",
-//                 "#FF0000",
-//                 "#007AFF"
-//             ],
-//         }]
-//     },
-//     options: {
-//         maintainAspectRatio: false,
-//     }
-// });
-
-//linechart
-
-
-// var ctx = document.getElementById("timeline-chart");
+var searchBox = document.querySelector(".search-term");
+searchBox.addEventListener('click', showTrending);
