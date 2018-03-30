6af85914dc926ecbde27ed576461914a3e475ab3
chamow97 <jchandramowli15039@it.ssn.edu.in>
2018-03-30 13:39:46 +0530

    Add

--- SIH/sentimentanalysis/urls.py
+++ SIH/sentimentanalysis/urls.py
@@ -4,4 +4,5 @@ from sentimentanalysis import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search$', views.search, name='search'),
+    url(r'^get_results', views.get_results, name='get_results'),
 ]
--- SIH/sentimentanalysis/views.py
+++ SIH/sentimentanalysis/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 def index(request):
     return render(request, "index.html")
 
 def search(request):
     return render(request, "search.html")
+@csrf_exempt
+def get_results(request):
+    searchTerm = request.POST.get('searchTerm');
+    return render(request, "results.html", {"searchTerm": searchTerm})
